9d8ae7965565a3278447beb1152f5de3ceb4cb3a
thelapssql@gmail.com <thelapssql@naver.com>
2021-09-16 20:37:29 +0900

    0916 20:37 commit

--- Seunggyun/app.py
+++ Seunggyun/app.py
@@ -1,41 +1,48 @@
+# app.py
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 from pymongo import MongoClient
 import jwt
-import datetime
 import hashlib
-from flask import Flask, render_template, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
 
+# mongoDB setting
+client = MongoClient('mongodb://13.125.91.65', 27017, username="test", password="test")
+db = client.muscle_course
+
 app = Flask(__name__)
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
 SECRET_KEY = 'SPARTA'
 
-client = MongoClient('mongodb://13.125.91.65', 27017, username="test", password="test")
-db = client.people
 
+# 로그인 페이지 랜더링
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+
+# 회원가입 페이지 랜더링
 @app.route('/member')
 def member():
     return render_template('member.html')
 
+
+# 메인 페이지 랜더링 (토큰 검사)
 @app.route('/')
 def home():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         user_info = db.users.find_one({"username": payload["id"]})
-        return render_template('index.html', user_info=user_info)
+        return render_template('user.html', user_info=user_info)
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+        return render_template('login.html')
 
-@app.route('/user/<username>')
+
+# 프로필 페이지 랜더링 (토큰 검사)
+@app.route('/')
 def user(username):
     # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
     token_receive = request.cookies.get('mytoken')
@@ -44,42 +51,49 @@ def user(username):
         status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
 
         user_info = db.users.find_one({"username": username}, {"_id": False})
-        return render_template('user.html', user_info=user_info, status=status)
+        return render_template('index.html', user_info=user_info, status=status)
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
 
+
+# 회원가입 기능 (클라에게 받은 정보 암호화(비번) 후 디비 저장)
 @app.route('/sign_up/save', methods=['POST'])
 def sign_up():
     username_receive = request.form['username_give']
     nickname_receive = request.form['nickname_give']
+    introduce_receive = request.form['introduce_give']
     password_receive = request.form['password_give']
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     doc = {
-        "username": username_receive,                               # 아이디
+        "username": username_receive,
         "nickname": nickname_receive,
-        "password": password_hash,                                  # 비밀번호
-        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
-        "profile_pic": "",                                          # 프로필 사진 파일 이름
-        "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
-        "profile_info": ""                                          # 프로필 한 마디
+        "introduce": introduce_receive,
+        "password": password_hash,
+        "profile_name": username_receive,
+        "profile_pic": "",
+        "profile_pic_real": "profile_pics/profile_placeholder.png"
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
 
-# 디비에서 아이디 중복 확인
+
+# 아이디 중복 확인 기능
 @app.route('/sign_up/check_id', methods=['POST'])
 def check_id():
     username_receive = request.form['username_give']
-    exists = bool(db.users.find_one({'username' : username_receive}))
-    return jsonify({'result' : 'success', 'exists': exists})
+    exists = bool(db.users.find_one({'username': username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
 
-# 디비에서 닉네임 중복 확인
+
+# 닉네임 중복 확인
 @app.route('/sign_up/check_nick', methods=['POST'])
 def check_nick():
     nickname_receive = request.form['nickname_give']
-    exists1 = bool(db.users.find_one({'nickname' : nickname_receive}))
-    return jsonify({'result' : 'success', 'exists1': exists1})
+    exists1 = bool(db.users.find_one({'nickname': nickname_receive}))
+    return jsonify({'result': 'success', 'exists1': exists1})
 
+
+# 로그인 기능 (클라에게 받은 정보(아이디/비번) 디비에 있으면 토큰 발급)
 @app.route('/sign_in', methods=['POST'])
 def sign_in():
     # 로그인
@@ -91,59 +105,17 @@ def sign_in():
 
     if result is not None:
         payload = {
-         'id': username_receive,
-         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
-
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
         return jsonify({'result': 'success', 'token': token})
     # 찾지 못하면
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
-# 메인 페이지의 포스팅 기능
-@app.route('/posting', methods=['POST'])
-def posting():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.users.find_one({"username": payload["id"]})
-        comment_receive = request.form["comment_give"]
-        date_receive = request.form["date_give"]
-        doc = {
-            "username": user_info["username"],
-            "profile_name": user_info["profile_name"],
-            "profile_pic_real": user_info["profile_pic_real"],
-            "comment": comment_receive,
-            "date": date_receive
-        }
-        db.posts.insert_one(doc)
-        return jsonify({"result": "success", 'msg': '포스팅 성공'})
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home"))
 
-@app.route("/get_posts", methods=['GET'])
-def get_posts():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        posts = list(db.posts.find({}).sort("date", -1).limit(20))
-        for post in posts:
-            post["_id"] = str(post["_id"])
-        return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.", 'posts': posts})
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home"))
-
-@app.route("/get_list", methods=['GET'])
-def get_list():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        lists = list(db.users.find({}, {'_id' : False}))
-        return jsonify({'result' : 'success', 'msg' : '유저를 가져왔습니다.', 'lists' : lists})
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home"))
-
+# 프로필 수정 기능 (클라에게 받은 정보(이름/이미지/소개) 업데이트)
 @app.route('/update_profile', methods=['POST'])
 def save_img():
     token_receive = request.cookies.get('mytoken')
@@ -154,20 +126,109 @@ def save_img():
         about_receive = request.form["about_give"]
         new_doc = {
             "profile_name": name_receive,
-            "profile_info": about_receive
+            "introduce": about_receive
         }
         if 'file_give' in request.files:
             file = request.files["file_give"]
             filename = secure_filename(file.filename)
             extension = filename.split(".")[-1]
             file_path = f"profile_pics/{username}.{extension}"
-            file.save("./static/"+file_path)
+            file.save("./static/" + file_path)
             new_doc["profile_pic"] = filename
             new_doc["profile_pic_real"] = file_path
-        db.users.update_one({'username': payload['id']}, {'$set':new_doc})
+        db.users.update_one({'username': payload['id']}, {'$set': new_doc})
         return jsonify({"result": "success", 'msg': '프로필을 업데이트했습니다.'})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
+
+
+###############################################################
+
+@app.route('/detail_page')
+def detail():
+    return render_template('detail.html')
+
+
+@app.route('/detail', methods=['GET'])
+def show_detail():
+    details = list(db.detail.find({}, {'_id': False}))
+    return jsonify({'all_detail': details})
+
+
+# 카드 저장
+@app.route('/detail', methods=['POST'])
+def save_detail():
+    title_receive = request.form['title_give']
+    content_receive = request.form['content_give']
+    file = request.files["file_give"]
+    comment = []
+
+    today = datetime.now()
+    mytime = today.strftime("%Y-%m-%d-%H-%M-%S")
+
+    filename = f"file-{mytime}"
+    save_to = f"static/{filename}.jpg"
+    file.save(save_to)
+
+    doc = {
+        'title': title_receive,
+        'content': content_receive,
+        'file': save_to,
+        'comment': comment
+    }
+    db.detail.insert_one(doc)
+    return jsonify({'msg': 'POST 요청 완료!'})
+
+
+# add_comment
+@app.route('/detail/add_comment', methods=['POST'])
+def add_comment():
+    # 추가할 카드의 ID 받기
+    imgSrc_receive = request.form['imgSrc_give']
+
+    # 코멘트를 추가한 사람의 name, nickname, content, date
+    nickname_receive = request.form['nickname_give']
+    comment_receive = request.form['comment_give']
+
+    today = datetime.now()
+    mytime = today.strftime("%Y-%m-%d-%H-%M-%S")
+
+    doc = {
+        'nickname': nickname_receive,
+        'comment': comment_receive,
+        'date': mytime
+    }
+    db.detail.update_one({'file': imgSrc_receive}, {'$push': {'comment': doc}})
+    return jsonify({'msg': 'Complete adding comment'})
+
+
+# modify_comment
+@app.route('/detail/modify_comment', methods=['POST'])
+def modify_comment():
+    # 추가할 카드의 ID 받기
+    imgSrc_receive = request.form['imgSrc_give']
+    idx_receive = request.form['idx_give']
+    content_receive = request.form['content_give']
+
+    target = db.detail.find_one({'file': imgSrc_receive})['comment']
+    target[int(idx_receive)]["comment"] = content_receive
+
+    db.detail.update({'file': imgSrc_receive}, {"$set": {"comment": target}})
+    return jsonify({'msg': 'Complete modifying comment'})
+
+
+# delete_comment
+@app.route('/detail/delete_comment', methods=['POST'])
+def delete_comment():
+    # 삭제할 댓글의 고유값 받기
+    imgSrc_receive = request.form['imgSrc_give']
+    idx_receive = request.form['idx_give']
+
+    updated_comment = db.detail.find_one({'file': imgSrc_receive}, {'_id': False})["comment"]
+    del updated_comment[int(idx_receive)]
+    db.detail.update({'file': imgSrc_receive}, {"$set": {"comment": updated_comment}})
+    return jsonify({'msg': 'Complete deleting comment'})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
--- Seunggyun/static/script.js
+++ Seunggyun/static/script.js
@@ -1,85 +1,7 @@
-function post() {
-    let comment = $("#textarea-post").val()
-    let today = new Date().toISOString()
-    $.ajax({
-        type: "POST",
-        url: "/posting",
-        data: {
-            comment_give: comment,
-            date_give: today
-        },
-        success: function (response) {
-            $("#modal-post").removeClass("is-active")
-            window.location.reload()
-        }
-    })
-}
-
-function time2str(date) {
-    let today = new Date()
-    let time = (today - date) / 1000 / 60  // 분
-
-    if (time < 60) {
-        return parseInt(time) + "분 전"
-    }
-    time = time / 60  // 시간
-    if (time < 24) {
-        return parseInt(time) + "시간 전"
-    }
-    time = time / 24
-    if (time < 7) {
-        return parseInt(time) + "일 전"
-    }
-    return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`
-}
-
-function get_posts() {
-    $("#post-box").empty()
-    $.ajax({
-        type: "GET",
-        url: "/get_posts",
-        data: {},
-        success: function (response) {
-            if (response["result"] == "success") {
-                let posts = response["posts"]
-                for (let i = 0; i < posts.length; i++) {
-                    let post = posts[i]
-                    let time_post = new Date(post["date"])
-                    let time_before = time2str(time_post)
-                    let html_temp = `<div class="box" id="${post["_id"]}">
-                                        <article class="media">
-                                            <div class="media-left">
-                                                <a class="image is-64x64" href="/user/${post['username']}">
-                                                    <img class="is-rounded" src="/static/${post['profile_pic_real']}"
-                                                         alt="Image">
-                                                </a>
-                                            </div>
-                                            <div class="media-content">
-                                                <div class="content">
-                                                    <p>
-                                                        <strong>${post['profile_name']}</strong> <small>@${post['username']}</small> <small>${time_before}</small>
-                                                        <br>
-                                                        ${post['comment']}
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </article>
-                                    </div>`
-                    $("#post-box").append(html_temp)
-                }
-            }
-        }
-    })
-}
-
-$(document).ready(function () {
-    get_posts()
-})
-
 function sign_out() {
     $.removeCookie('mytoken', {path: '/'});
-    alert('로그아웃!')
-    window.location.href = "/login"
+    alert('로그아웃!');
+    window.location.href = '/';
 }
 
 function update_profile() {
@@ -109,5 +31,188 @@ function update_profile() {
     });
 }
 
-document.getElementById('btns-me').style.color = 'red';
-document.getElementById('join').style.color = 'red';
+function is_nickname(asValue) {
+    var regExp = /^(?=.*[a-zA-Z])[-a-zA-Z0-9_.]{5,20}$/;
+    return regExp.test(asValue);
+}
+
+function is_nickname2(asValue) {
+    var regExp = /^[가-힣]{2,4}$/;
+    return regExp.test(asValue)
+}
+
+function is_password(asValue) {
+    var regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{5,20}$/;
+    return regExp.test(asValue);
+}
+
+function check_id() {
+    let username = $("#input-username").val()
+
+    console.log(username)
+    if (username == "") {
+        $("#help-id").text("아이디 확인 5~20자 이상").removeClass(" is-safe").addClass("is-danger")
+        $("#input-username").focus()
+        return;
+    }
+    if (!is_nickname(username)) {
+        $("#help-id").text("아이디 확인 5~20자 이상").removeClass("is-safe").addClass("is-danger")
+        $("#input-username").focus()
+        return;
+    }
+    $("#help-id").addClass("is-loading")
+    $.ajax({
+        type: "POST",
+        url: "/sign_up/check_id",
+        data: {
+            username_give: username
+        },
+        success: function (response) {
+
+            if (response["exists"]) {
+                $("#help-id").text("이미 존재하는 아이디입니다.").removeClass("is-safe").addClass("is-danger")
+                $("#input-username").focus()
+            } else {
+                $("#help-id").text("사용할 수 있는 아이디입니다.").removeClass("is-danger").addClass("is-success")
+            }
+            $("#help-id").removeClass("is-loading")
+
+        }
+    });
+}
+
+function check_nick() {
+    let nickname = $("#input-nickname").val()
+
+    if (nickname == "") {
+        $("#help-nick").text("닉네임 확인 2~4자 한글").removeClass(" is-safe").addClass("is-danger")
+        $("#input-nickname").focus()
+        return;
+    }
+    if (!is_nickname2(nickname)) {
+        $("#help-nick").text("닉네임 확인 2~4자 한글").removeClass("is-safe").addClass("is-danger")
+        $("#input-nickname").focus()
+        return;
+    }
+
+    $("#help-nick").addClass("is-loading")
+    $.ajax({
+        type: "POST",
+        url: "/sign_up/check_nick",
+        data: {
+            nickname_give: nickname
+        },
+        success: function (response) {
+
+            if (response["exists1"]) {
+                $("#help-nick").text("이미 존재하는 닉네임 입니다.").removeClass("is-safe").addClass("is-danger")
+                $("#input-nickname").focus()
+            } else {
+                $("#help-nick").text("사용할 수 있는 닉네임 입니다.").removeClass("is-danger").addClass("is-success")
+            }
+            $("#help-nick").removeClass("is-loading")
+
+        }
+    });
+}
+
+// 회원가입 요청
+function sign_up() {
+    let username = $("#input-username").val()
+    let nickname = $('#input-nickname').val()
+    let introduce = $('#input-introduce').val()
+    let password = $("#input-password").val()
+    let password2 = $("#input-password2").val()
+
+    if ($("#help-id").hasClass("is-danger")) {
+        alert("아이디를 다시 확인해주세요.")
+        return;
+    } else if (!$("#help-id").hasClass("is-success")) {
+        alert("아이디 중복확인을 해주세요.")
+        return;
+    }
+
+    if ($("#help-nick").hasClass("is-danger")) {
+        alert("닉네임을 다시 확인해주세요.")
+        return;
+    } else if (!$("#help-nick").hasClass("is-success")) {
+        alert("닉네임 중복확인을 해주세요.")
+        return;
+    }
+
+    if (password == "") {
+        $("#help-password").text("비밀번호를 입력해주세요.").removeClass("is-safe").addClass("is-danger")
+        $("#input-password").focus()
+        return;
+    } else if (!is_password(password)) {
+        $("#help-password").text("비밀번호의 형식을 확인해주세요. 영문과 숫자 필수 포함, 특수문자(!@#$%^&*) 사용가능 8-20자").removeClass("is-safe").addClass("is-danger")
+        $("#input-password").focus()
+        return
+    } else {
+        $("#help-password").text("사용할 수 있는 비밀번호입니다.").removeClass("is-danger").addClass("is-success")
+    }
+    if (password2 == "") {
+        $("#help-password2").text("비밀번호를 입력해주세요.").removeClass("is-safe").addClass("is-danger")
+        $("#input-password2").focus()
+        return;
+    } else if (password2 != password) {
+        $("#help-password2").text("비밀번호가 일치하지 않습니다.").removeClass("is-safe").addClass("is-danger")
+        $("#input-password2").focus()
+        return;
+    } else {
+        $("#help-password2").text("비밀번호가 일치합니다.").removeClass("is-danger").addClass("is-success")
+    }
+    $.ajax({
+        type: "POST",
+        url: "/sign_up/save",
+        data: {
+            username_give: username,
+            password_give: password,
+            nickname_give: nickname,
+            introduce_give: introduce
+        },
+        success: function (response) {
+            alert("회원가입을 축하드립니다!")
+            window.location.replace("/login")
+        }
+    });
+
+}
+
+function sign_in() {
+    let username = $("#input-username").val()
+    let password = $("#input-password").val()
+
+    if (username == "") {
+        $("#help-id-login").text("아이디를 입력해주세요.")
+        $("#input-username").focus()
+        return;
+    } else {
+        $("#help-id-login").text("")
+    }
+
+    if (password == "") {
+        $("#help-password-login").text("비밀번호를 입력해주세요.")
+        $("#input-password").focus()
+        return;
+    } else {
+        $("#help-password-login").text("")
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "/sign_in",
+        data: {
+            username_give: username,
+            password_give: password
+        },
+        success: function (response) {
+            if (response['result'] == 'success') {
+                $.cookie('mytoken', response['token'], {path: '/'});
+                window.location.replace('/')
+            } else {
+                alert(response['msg'])
+            }
+        }
+    });
+}
